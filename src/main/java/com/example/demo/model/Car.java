87ecd2d37dc6e2db5cff62857c88a3d65d77262f
LaurentMarc <59912178+LaurentMarc@users.noreply.github.com>
2020-12-11 13:50:17 +0100

    ajout database H2

--- src/main/java/com/example/demo/model/Car.java
+++ src/main/java/com/example/demo/model/Car.java
@@ -1,6 +1,13 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
+	@Id
+	@GeneratedValue  // auto incrémente id lors création voiture
 	private int id;
     private String nom;
     private String couleur;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO CAR(id, nom, couleur) VALUES(1, '405 T16' , 'rouge');
+INSERT INTO CAR(id, nom, couleur) VALUES(2, 'K2000' , 'noire');
+INSERT INTO CAR(id, nom, couleur) VALUES(3, 'Tractopelle' , 'jaune');
